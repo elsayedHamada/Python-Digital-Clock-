ba1a3d776970b4ca4fedc943102040c49f708dd7
Elsayed H Elsory <elsayed.7amada@gmail.com>
2021-06-28 10:04:05 +0200

    Finish The Digital Clock⌚ It an easy project But It's a good Practise

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # Import Needed Modules
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title("Digital Clock")
+
+
+def time():
+    time_string = strftime(" %H:%M:%S %p ")
+    label.config(text=time_string)
+    label.after(1000, time)
+
+
+label = Label(root, font=("ds_digital", 100), background="black", foreground="Purple")
+label.pack(anchor="center")
+
+time()
+mainloop()
