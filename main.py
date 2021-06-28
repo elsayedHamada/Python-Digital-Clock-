be4be772aa316feeb1b1668bb5e96fbb0498df57
Elsayed H Elsory <elsayed.7amada@gmail.com>
2021-06-28 10:35:16 +0200

    Last Touches👌

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # Import Needed Modules
-from tkinter import *
+import tkinter as tk
 from tkinter.ttk import *
 from time import strftime
 # Create The MAin Window
-root = Tk()
+root = tk.Tk()
+# Var
+WIDTH = 700
+HEIGHT = 400
 # set title
 root.title("Digital Clock")
+# set icon
+root.iconphoto(False, tk.PhotoImage(file="logo.png"))
+# set Back
+root.configure(bg="Black")
+# set size
+root.geometry(f"{WIDTH}x{HEIGHT}")
 
 
 # Time Function
@@ -16,9 +25,9 @@ def time():
 
 
 # Add To Screen
-label = Label(root, font=("ds_digital", 100), background="black", foreground="Purple")
-label.pack(anchor="center")
+label = Label(root, font=("DS-Digital", 100), background="black", foreground="#25ee00")
+label.place(x=30, y=120)
 # Call The Function
 time()
 # Close The Loop
-mainloop()
+tk.mainloop()
